7df4a5ec0b1ea87ca58e1cb0efd407ffc6fc2527
iotkursp3 <94227565+iotkursp3@users.noreply.github.com>
2021-11-19 14:17:32 +0100

    Create led_1_blink.ino

---
+++ Blink/led_1_blink/led_1_blink.ino
@@ -0,0 +1,17 @@
+// Koristićemo varijablu ledPin da označimo pin na koji ćemo povezati LED diodu
+// varijabli ledPin dajemo vrijednost 23 (pin 23 na ESP32 pločici)
+const int ledPin = 23;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin ledPin as an output.
+  pinMode(ledPin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                  // wait for a second
+}
