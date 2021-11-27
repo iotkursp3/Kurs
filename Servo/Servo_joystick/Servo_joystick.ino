d0b0f929a6358a03e6e19a55006ef17da2a52f95
iotkursp3 <94227565+iotkursp3@users.noreply.github.com>
2021-11-27 14:38:20 +0100

    Create Servo_joystick.ino

---
+++ Servo/Servo_joystick/Servo_joystick.ino
@@ -0,0 +1,33 @@
+// These constants won't change. They're used to give names to the pins used:
+const int xAxisPin = 25;
+const int yAxisPin = 26;
+
+Servo servo1;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(xAxisPin, INPUT);
+  pinMode(yAxisPin, INPUT);
+  servo1.attach(servoPin);
+}
+
+void loop() {
+  // read the analog in value:
+  int xAxisValue = 0;        // value read from the pot
+  int yAxisValue = 0;  
+  
+  xAxisValue = analogRead(xAxisPin);
+  yAxisValue = analogRead(yAxisPin);
+
+  // print the results to the Serial Monitor:
+  Serial.print("X: ");
+  Serial.print(xAxisValue);
+  Serial.print("  Y: ");
+  Serial.print(yAxisValue);
+  
+  int servoPosition = map(xAxisValue, 0, 4096, 0, 180);
+  servo1.write(servoPosition);
+  Serial.println(servoPosition);
+  
+  delay(2);
+}
