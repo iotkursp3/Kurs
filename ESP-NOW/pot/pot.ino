cfd980ac68018c5ad8bd220e49c3ceb943225f5d
iotkursp3 <94227565+iotkursp3@users.noreply.github.com>
2021-12-18 17:38:01 +0100

    Create pot.ino

---
+++ ESP-NOW/pot/pot.ino
@@ -0,0 +1,138 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+const int pot_pin = 13;     // Digital pin connected to the DHT sensor
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// REPLACE WITH THE MAC Address of your receiver 
+uint8_t broadcastAddress[] = {0x58, 0xBF, 0x25, 0x16, 0xB9, 0xA8};
+
+// Define variables to store DHT11 readings to be sent
+int pot_value;
+
+// Define variables to store incoming readings
+int incoming_pot_value;
+
+// Variable to store if sending data was successful
+String success;
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct struct_message {
+    int value;
+} struct_message;
+
+// Create a struct_message called DHT11Readings to hold sensor readings
+struct_message pot_readings;
+
+// Create a struct_message to hold incoming sensor readings
+struct_message incomingReadings;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    success = "Delivery Success :)";
+  }
+  else{
+    success = "Delivery Fail :(";
+  }
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  incoming_pot_value = incomingReadings.value;
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  pinMode(pot_pin, INPUT);
+  
+  // Init OLED display
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+  getReadings();
+ 
+  // Set values to send
+  pot_readings.value = pot_value;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &pot_readings, sizeof(pot_readings));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  updateDisplay();
+  delay(1000);
+}
+
+void getReadings(){
+  pot_value = analogRead(pot_pin);
+}
+
+void updateDisplay(){
+  // Display Readings on OLED Display
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("INCOMING READINGS");
+  display.setCursor(0, 15);
+  display.print("pot_value: ");
+  display.print(incoming_pot_value);
+  display.setCursor(0, 25);
+  display.print("Your pot_value: ");
+  display.print(pot_value);
+  display.setCursor(0, 35);
+  display.print(success);
+  display.display();
+}
