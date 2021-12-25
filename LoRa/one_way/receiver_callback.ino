f040ef5fd7ccb2fb4c3c7a1eef1342db162fc4df
iotkursp3 <94227565+iotkursp3@users.noreply.github.com>
2021-12-25 14:14:31 +0100

    Create receiver_callback.ino

---
+++ LoRa/one_way/receiver_callback.ino
@@ -0,0 +1,50 @@
+#include "LoRa.h"
+#include "ArduinoJson.h"
+
+#define SCK 18
+#define MISO 19
+#define MOSI 23
+#define SS 5
+#define RST 14
+#define DIO0 2
+
+#define BAND 433E6
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+
+  //SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+
+  if (!LoRa.begin(BAND)){
+    Serial.println("LoRa init failed. Check your connections.");
+    delay(500);
+    ESP.restart();
+  }
+  // register the receive callback
+  LoRa.onReceive(onReceive);
+
+  // put the radio into receive mode
+  LoRa.receive();
+}
+
+void loop() {
+  // do nothing
+}
+
+void onReceive(int packetSize) {
+  // received a packet
+  Serial.print("Received packet '");
+
+  // read packet
+  for (int i = 0; i < packetSize; i++) {
+    Serial.print((char)LoRa.read());
+  }
+
+  // print RSSI of packet
+  Serial.print("' with RSSI ");
+  Serial.println(LoRa.packetRssi());
+}
